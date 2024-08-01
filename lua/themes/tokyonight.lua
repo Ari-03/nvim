980e9ae98a2cfeb6b59cad7146ca57f728c670e4
Mac.Ari <d.aritra@outlook.com>
2024-08-01 13:22:09 -0700

    Current nvim config

---
+++ lua/themes/tokyonight.lua
@@ -0,0 +1,11 @@
+return {
+    { -- you can easily change to a different colorscheme.
+        -- change the name of the colorscheme plugin below, and then
+        -- change the command in the config to whatever the name of that colorscheme is.
+        --
+        -- if you want to see what colorschemes are already installed, you can use `:telescope colorscheme`.
+        'folke/tokyonight.nvim',
+        priority = 1000, -- make sure to load this before all the other start plugins.
+    },
+}
+-- vim: ts=2 sts=2 sw=2 et
